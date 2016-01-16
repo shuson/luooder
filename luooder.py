4bd4a6dd5b1df2ff1c0248ed0dd08bb9ea91cc12
nevermoi <wlgq@hotmail.com>
2016-01-16 10:38:31 +0800

    fix encoding issue and make it colorful

--- luooder.py
+++ luooder.py
@@ -2,6 +2,7 @@
 # date: 15-Jan-2016
 
 import os
+import sys
 import requests
 #import songdetails
 import unicodedata
@@ -15,6 +16,19 @@ TRACK_NAME = "{} -{}.mp3"
 
 proxies = {}
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def full2half(uc):
     """convert full-width character to half-width one
     """
@@ -27,13 +41,14 @@ def get_song_list(volumn):
     bs = BeautifulSoup(r.content, 'html.parser')
     songs = bs.find_all('div', 'player-wrapper')
 
-    print "song list of volumn {}".format(volumn)
+    print bcolors.OKBLUE + "song list of volumn {}:".format(volumn) + bcolors.ENDC
     result = []
     for song in songs:
         meta = {}
         meta['name'] = song.find('p', 'name').getText()
         meta['artist'] = song.find('p', 'artist').getText()[7:]
         meta['album'] = song.find('p', 'album').getText()[6:]
+        print bcolors.UNDERLINE + '{} by {}'.format(meta['name'], meta['artist']) + bcolors.ENDC
         result.append(meta)
 
     return result
@@ -41,14 +56,14 @@ def get_song_list(volumn):
 def download_songs(volumn):
     songs = get_song_list(volumn)
     if len(songs) == 0:
-        print 'please make sure the volumn exists in luoo.net'
+        print bcolors.WARNING + 'please make sure the volumn exists in luoo.net' + bcolors.ENDC
         return
 
     if not os.path.exists(str(volumn)):
         os.makedirs(str(volumn))
         os.chdir(str(volumn))
         
-    print 'downloading...'
+    print bcolors.OKBLUE + 'downloading...' + bcolors.ENDC
     index = 0
     for song in songs:
         index += 1
@@ -60,15 +75,15 @@ def download_songs(volumn):
             for chunk in r.iter_content():
                 fd.write(chunk)
             fd.close()
-        print "{} is downloaded.".format(song['name'])
+        print bcolors.OKGREEN + "{} is downloaded.".format(song['name']) + bcolors.ENDC
 
 if __name__ == '__main__':
 
     while True:
-        print "input the volumn number to download all songs within it!\n>"
+        print bcolors.HEADER + "input the volumn number to download all songs within it!\n>" + bcolors.ENDC
         vol = raw_input()
         if str(vol).isdigit():
-            print 'initiating...'
+            print bcolors.BOLD + 'initiating...' + bcolors.ENDC
             download_songs(vol)
             break
 
